c366289c23e6706f23512ee6c3ae779c90228200
Awwabsiddiqui <awwab9718.com>
2024-02-06 20:56:01 +0530

    update git status

---
+++ src/main/java/com/example/springrest/controller/SpringSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.example.springrest.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+//	@Bean
+//	public BCryptPasswordEncoder passwordEncoder() {
+//		return new BCryptPasswordEncoder();
+//	}
+
+    @Bean
+    DaoAuthenticationProvider authenticationProvider() {
+    	DaoAuthenticationProvider DaoAP = new DaoAuthenticationProvider();
+    	DaoAP.setUserDetailsPasswordService(null);
+			return DaoAP;
+			
+		}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests();
+	}
+}

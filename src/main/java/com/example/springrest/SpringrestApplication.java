5b36118b4a9348438ebe385b2b0b97d12a963598
Awwabsiddiqui <awwab9718.com>
2024-02-06 21:05:15 +0530

    remove check

--- src/main/java/com/example/springrest/SpringrestApplication.java
+++ src/main/java/com/example/springrest/SpringrestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringrestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringrestApplication.class, args);//check git alive stat
+		SpringApplication.run(SpringrestApplication.class, args);
 	}
 
 }
